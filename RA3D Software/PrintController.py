a2cba66b73444cbd8102431431a7098e782f2e70
Justin <justinfauson@comcast.net>
2026-02-06 20:00:07 -0700

    Improved Serial Processing

--- RA3D Software/PrintController.py
+++ RA3D Software/PrintController.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from tkinter import filedialog
-<<<<<<< HEAD
 import os, re, copy, threading
 from ArmController import Position, Origin, MoveCommand,MoveParameters
 class PrintController:
@@ -215,7 +214,7 @@ class PrintController:
             return
         # Send the command to the arm
         # TODO Find/create a better move command, consider using moveG/drivemotorsG for gcode
-<<<<<<< HEAD
+
         self.root.armController.sendML(X=point[0], Y=point[1], Z=point[2], Rx=point[3], Ry=point[4], Rz=point[5], MoveParameters=self.printParemeters)
     def syncOrigin(self):
         self.origin = self.root.armController.origin

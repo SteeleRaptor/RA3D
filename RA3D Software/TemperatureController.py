191a29cc925bda4784b8540baa27d1ca6aeec0ad
Cody Blough <codyberthoud@gmail.com>
2026-04-01 12:29:11 -0600

    Fixed GPIO error in TemperatureController

--- RA3D Software/TemperatureController.py
+++ RA3D Software/TemperatureController.py
@@ -188,7 +188,7 @@ class TemperatureController:
             else:
                 # Turn the hotend heater off
                 self.hotendHeaterOn = False
-                self.output(self.hoteendPin, GPIO.LOW)
+                GPIO.output(self.hoteendPin, GPIO.LOW)
         else:
             # Drive the hotend signal low in case it is on when control flag disabled
             GPIO.output(self.hotendPin, GPIO.LOW)
